daf30fc4e339353a5f21f8a6093d9657e0bbf4ac
Jafie <fortun.axel@gmail.com>
2021-06-18 22:32:28 +0200

    Projectile available with limit under game zone

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,18 @@
 mod moveable_sprites;
 use crate::moveable_sprites::MoveableSprite;
-use crate::moveable_sprites::ArmedSprite;
+use crate::moveable_sprites::Weapon;
+
+
+// Game area limit
+static GAME_AREA_LIMIT_X: f32 = 500.0;
+static GAME_AREA_LIMIT_Y: f32 = 300.0;
+
+
+// Main character initialization
+static INITIAL_POST_MAIN_CHAR_X: f32 = 0.0;
+static INITIAL_POST_MAIN_CHAR_Y: f32 = -215.0;
+static INITIAL_SPEED_MAIN_CHAR: f32 = 200.0;
+static INITIAL_DIRECTION_MAIN_CHAR: (f32, f32) = (0.0, 1.0);
 
 use bevy::{
     prelude::*,
@@ -12,13 +24,14 @@ fn main() {
         .add_plugins(DefaultPlugins)
         .insert_resource(WindowDescriptor {
             title: "street_of_zombies".to_string(),
-            width: 500.,
-            height: 300.,
+            width: GAME_AREA_LIMIT_X,
+            height: GAME_AREA_LIMIT_Y,
             ..Default::default()
         })
         .add_startup_system(setup.system())
         .add_startup_system(set_window_title.system())
         .add_system(keyboard_capture.system())
+        .add_system(projectile_movement_system.system())
         .add_system(fire_capture.system())
         .run();
 }
@@ -36,11 +49,11 @@ fn setup(
     commands
         .spawn_bundle(SpriteBundle {
             material: materials.add(Color::rgb(0.3, 0.0, 1.0).into()),
-            transform: Transform::from_xyz(0.0, -215.0, 0.0),
+            transform: Transform::from_xyz(INITIAL_POST_MAIN_CHAR_X, INITIAL_POST_MAIN_CHAR_Y, 0.0),
             sprite: Sprite::new(Vec2::new(30.0, 30.0)),
             ..Default::default()
         })
-        .insert(moveable_sprites::MainCharacter { speed: 500.0, direction: (1.0, 0.0) });
+        .insert(moveable_sprites::MainCharacter::new(INITIAL_SPEED_MAIN_CHAR, INITIAL_DIRECTION_MAIN_CHAR, (INITIAL_POST_MAIN_CHAR_X, INITIAL_POST_MAIN_CHAR_Y)));
 }
 
 /// This system will then change the title during execution
@@ -53,9 +66,9 @@ fn set_window_title(mut windows: ResMut<Windows>) {
 fn keyboard_capture(
     time: Res<Time>,
     keyboard_input: Res<Input<KeyCode>>,
-    mut query: Query<(&moveable_sprites::MainCharacter, &mut Transform)>,
+    mut query: Query<(&mut moveable_sprites::MainCharacter, &mut Transform)>,
 ) {
-    if let Ok((main_character, mut transform)) = query.single_mut() {
+    if let Ok((mut main_character, mut transform)) = query.single_mut() {
         let mut direction : (f32, f32) = (0.0, 0.0);
         let mut number_of_valid_pressure : u8 = 0;
 
@@ -94,11 +107,35 @@ fn fire_capture(
     mut commands: Commands,
     mut materials: ResMut<Assets<ColorMaterial>>,
     keyboard_input: Res<Input<KeyCode>>,
-    mut query: Query<(&moveable_sprites::MainCharacter, &mut Transform)>,
+    mut query: Query<(&mut moveable_sprites::MainCharacter, &mut Transform)>,
 ) {
-    if let Ok((main_character, _)) = query.single_mut() {
+    if let Ok((mut main_character, _)) = query.single_mut() {
         if keyboard_input.pressed(KeyCode::Space) {
-            main_character.fire_projectile(&mut commands, &mut materials, main_character.direction);
+            main_character.fire(&mut commands, &mut materials);
+        }
+        else {
+            main_character.reload();
+        }
+    }
+}
+
+// Fire projectiles
+fn projectile_movement_system(
+    mut commands: Commands,
+    time: Res<Time>,
+    mut projectile_query: Query<(&mut moveable_sprites::Projectile,
+    &mut Transform, Entity)>) {
+
+    for projectile_single_query in projectile_query.iter_mut() {
+        let (mut projectile, mut transform, projectile_entity) = projectile_single_query;
+        let direction_of_fire = projectile.get_direction();
+        projectile.move_sprite(&time, &direction_of_fire, &mut transform.translation);
+
+        let (proj_x, proj_y) = projectile.get_position();
+
+        // If outside of game area, delete
+        if proj_x.abs() > GAME_AREA_LIMIT_X || proj_y.abs() > GAME_AREA_LIMIT_Y {
+            commands.entity(projectile_entity).despawn();
         }
     }
 }
--- src/moveable_sprites.rs
+++ src/moveable_sprites.rs
@@ -5,62 +5,129 @@ use bevy::{
 
 // Traits definition
 pub trait MoveableSprite {
+    fn new(speed: f32, direction: (f32, f32), initial_pos: (f32, f32)) -> Self;
     fn get_speed(&self) -> f32;
+    fn get_direction(&self) -> (f32, f32);
     fn set_new_direction(&mut self, direction: (f32, f32));
-    fn move_sprite(&self, time: &Res<Time>, direction: &(f32, f32), translated_movement: &mut bevy::prelude::Vec3) {
+    fn get_position(&self) -> (f32, f32);
+    fn set_new_position(&mut self, position: (f32, f32));
+    fn move_sprite(&mut self, time: &Res<Time>, direction: &(f32, f32), translated_movement: &mut bevy::prelude::Vec3) {
         // move the sprite
         translated_movement.x += time.delta_seconds() * direction.0 * &self.get_speed();
         translated_movement.y += time.delta_seconds() * direction.1 * &self.get_speed();
+        self.set_new_direction(*direction);
+        self.set_new_position((translated_movement.x, translated_movement.y));
     }
+    fn get_sprite_name() -> String;
 }
 
-pub trait ArmedSprite {
-    fn fire_projectile(&self,
+pub trait Weapon {
+    fn fire_global(&mut self,
         commands: &mut Commands,
         materials: &mut ResMut<Assets<ColorMaterial>>,
-        direction: (f32, f32)) {
-            
-        commands
-        .spawn_bundle(SpriteBundle {
-            material: materials.add(Color::rgb(0.3, 0.0, 1.0).into()),
-            transform: Transform::from_xyz(0.0, -215.0, 0.0),
-            sprite: Sprite::new(Vec2::new(5.0, 5.0)),
-            ..Default::default()
-        })
-        .insert(Projectile { speed: 500.0, direction_factor: direction });
+        direction: (f32, f32),
+        initial_pos: (f32, f32)) {
+            if self.get_amo() > 0 {
+            let (pos_x, pox_y) = initial_pos;
+            commands
+            .spawn_bundle(SpriteBundle {
+                material: materials.add(Color::rgb(0.3, 0.0, 1.0).into()),
+                transform: Transform::from_xyz(pos_x, pox_y, 0.0),
+                sprite: Sprite::new(Vec2::new(5.0, 5.0)),
+                ..Default::default()
+            })
+            .insert(Projectile::new(500.0, direction, initial_pos));
+            self.reduce_amo();
+        }
     }
+    fn reload(&mut self);
+    fn get_amo(&self) -> u32;
+    fn reduce_amo(&mut self);
 }
+
 
 // The main character, moveable by user
 pub struct MainCharacter {
-    pub speed: f32,
-    pub direction: (f32, f32)
+    speed: f32,
+    current_position : (f32, f32),
+    direction: (f32, f32),
+    amo : u32
 }
 
 impl MoveableSprite for MainCharacter {
+    fn new(speed_to_set: f32, direction_to_set: (f32, f32), initial_pos: (f32, f32)) -> Self {
+        MainCharacter { speed: speed_to_set, current_position: initial_pos, direction: direction_to_set, amo: 1 }
+    }
     fn get_speed(&self) -> f32 {
         self.speed
     }
     fn set_new_direction(&mut self, direction: (f32, f32)) {
         self.direction = direction;
     }
-
+    fn get_direction(&self) -> (f32, f32) {
+        self.direction
+    }
+    fn get_position(&self) -> (f32, f32) {
+        self.current_position
+    }
+    fn set_new_position(&mut self, position: (f32, f32)) {
+        println!("New position of {} is equal to {:?}", MainCharacter::get_sprite_name(), self.current_position);
+        self.current_position = position;
+    }
+    fn get_sprite_name() -> String {
+        String::from("Josay")
+    }
 }
 
-impl ArmedSprite for MainCharacter {
+impl Weapon for MainCharacter {
+    fn reload(&mut self) {
+        self.amo = 1;
+    }
+    fn get_amo(&self) -> u32 {
+        self.amo
+    }
+    fn reduce_amo(&mut self) {
+        self.amo -= 1;
+    }
+}
+
+impl MainCharacter {
+    pub fn fire(&mut self,
+        commands: &mut Commands,
+        materials: &mut ResMut<Assets<ColorMaterial>>) {
+            self.fire_global(commands, materials, self.get_direction(), self.get_position());
+    }
 }
 
 // PROJECTILE DEFINITION
 pub struct Projectile {
     speed: f32,
-    direction_factor: (f32, f32)
+    direction: (f32, f32),
+    current_position : (f32, f32)
 }
 
 impl MoveableSprite for Projectile {
+    fn new(speed_to_set: f32, direction_to_set: (f32, f32), initial_pos: (f32, f32)) -> Self {
+        Projectile { speed: speed_to_set, direction: direction_to_set,  current_position: initial_pos}
+    }
+
     fn get_speed(&self) -> f32 {
         self.speed
     }
     fn set_new_direction(&mut self, direction: (f32, f32)) {
-        self.direction_factor = direction;
+        self.direction = direction;
+    }
+    fn get_direction(&self) -> (f32, f32) {
+        self.direction
+    }
+    fn get_position(&self) -> (f32, f32) {
+        self.current_position
+    }
+    fn set_new_position(&mut self, position: (f32, f32)) {
+        println!("New position of {} is equal to {:?}", Projectile::get_sprite_name(), self.current_position);
+        self.current_position = position;
+    }
+    fn get_sprite_name() -> String {
+        String::from("Amo")
     }
 }
